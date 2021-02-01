d9216a0f17f5d94a9f0742c1170908c53218c3e6
Akshay Nair <akshaynairpanna@gmail.com>
2021-02-01 18:41:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ app.post('/compilecode', function (req, res) {
         console.log(response.body);
         var my_output = JSON.parse(response.body);
         if(my_output.run_status.status != 'AC'){
-            actualoutput = "Please check the Code and language selected.";
+            actualoutput = "Please Check the language selected and \n" + my_output.run_status.status_detail;
         actualinput = input;
         actualcode = code;
         actualtime = my_output.run_status.time_used;
